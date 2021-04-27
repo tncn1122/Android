0b11d5ab2471d1900d76a8b4cd38ee5ace387127
tncn1122 <tncnhan@gmail.com>
2021-04-28 00:06:38 +0700

    - thêm nút thêm sinh viên

--- app/src/main/java/com/tncnhan/android_giuaki/DSSVNhapDiemActivity.java
+++ app/src/main/java/com/tncnhan/android_giuaki/DSSVNhapDiemActivity.java
@@ -1,7 +1,10 @@
 package com.tncnhan.android_giuaki;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.util.Pair;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.Cursor;
@@ -15,12 +18,20 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class DSSVNhapDiemActivity extends AppCompatActivity {
 
     SharedPreferences sharedPreferences;
     DBHelper DBhelper;
     ArrayList<Diem> dsDiem  = new ArrayList<>();
+    String[] AlertDialogItems;
+    boolean[] Selectedtruefalse;
+
+    AlertDialog.Builder alertdialogbuilder;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,12 +40,18 @@ public class DSSVNhapDiemActivity extends AppCompatActivity {
         sharedPreferences = getSharedPreferences("dataLogin",MODE_PRIVATE);
         int role= sharedPreferences.getInt("role",0);
         ImageButton imgbtn_addDiem= findViewById(R.id.btnThem);
+        ImageButton imgbtn_addSV = findViewById(R.id.addSVbtn);
+        ImageButton imgbtn_editClass = findViewById(R.id.classEditBtn);
         EditText NhapDiem = findViewById(R.id.edtDiem);
 
         if(role == 1){
-            imgbtn_addDiem.setVisibility(View.INVISIBLE);
+            imgbtn_editClass.setVisibility(View.VISIBLE);
+            imgbtn_addSV.setVisibility(View.VISIBLE);
+            imgbtn_addDiem.setVisibility(View.GONE);
         }else if(role == 0){
             imgbtn_addDiem.setVisibility(View.VISIBLE);
+            imgbtn_editClass.setVisibility(View.INVISIBLE);
+            imgbtn_addSV.setVisibility(View.INVISIBLE);
         }
         Intent intent = getIntent();
         String message = intent.getStringExtra("message");
@@ -46,7 +63,7 @@ public class DSSVNhapDiemActivity extends AppCompatActivity {
         dt= DBhelper.GetData("select student.studentid, student.studentname, score.score, class.classname from (student inner join score on student.studentid = score.studentid) inner join class on score.classid = class.classid  where class.classid = '" + message + "'" );
         while(dt.moveToNext())
         {
-            Log.d("MSV", dt.getString(0) + " " + dt.getString(2));
+            //Log.d("MSV", dt.getString(0) + " " + dt.getString(2));
             SinhVien sv = new SinhVien(dt.getString(0), dt.getString(1));
             MonHoc mh = new MonHoc(message, dt.getString(3),0,null);
             Float score;
@@ -66,7 +83,7 @@ public class DSSVNhapDiemActivity extends AppCompatActivity {
 
         TextView teacher = findViewById(R.id.txtTeacher);
         teacher.setText("Giảng Viên: " + getTeacherOfClass(message));
-        Log.d("MSVVV", String.valueOf(adapter.getCount()));
+        //Log.d("MSVVV", String.valueOf(adapter.getCount()));
         ListView listView= findViewById(R.id.lvDSDiem);
         listView.setAdapter(adapter);
 
@@ -88,6 +105,74 @@ public class DSSVNhapDiemActivity extends AppCompatActivity {
             }
         });
 
+        // thêm sv vào lớp
+        Map<String, Pair<String, Boolean>> studentInClass = getStudentInClass(message);
+        Selectedtruefalse = new boolean[studentInClass.size()];
+        ArrayList<String> studentId = new ArrayList<>();
+        AlertDialogItems = new String[studentInClass.size()];
+        int mapIndex = 0;
+        for(Map.Entry<String, Pair<String, Boolean>> entry : studentInClass.entrySet()){
+            Selectedtruefalse[mapIndex] = entry.getValue().second;
+            studentId.add(entry.getKey());
+            AlertDialogItems[mapIndex] = entry.getKey() + " - " + entry.getValue().first;
+
+
+            mapIndex++;
+        }
+
+        imgbtn_addSV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                alertdialogbuilder = new AlertDialog.Builder(DSSVNhapDiemActivity.this);
+
+                alertdialogbuilder.setMultiChoiceItems(AlertDialogItems, Selectedtruefalse, new DialogInterface.OnMultiChoiceClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+
+                    }
+                });
+
+                alertdialogbuilder.setCancelable(false);
+
+                alertdialogbuilder.setTitle("Chọn Sinh Viên");
+
+                alertdialogbuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        int a = 0;
+                        while(a < Selectedtruefalse.length)
+                        {
+                            boolean value = Selectedtruefalse[a];
+
+                            if(value){
+                                addStudentIntoClass(studentId.get(a), message);
+                                Log.d("TESTALERT", studentId.get(a) + "--" + message);
+                            }
+                            else{
+                                removeStudentFromClass(studentId.get(a), message);
+                            }
+
+                            a++;
+                        }
+                        finish();
+                        startActivity(getIntent());
+                    }
+
+                });
+
+                alertdialogbuilder.setNeutralButton("Hủy", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                    }
+                });
+
+                AlertDialog dialog = alertdialogbuilder.create();
+
+                dialog.show();
+            }
+        });
+
     }
 
     private String getTeacherOfClass(String classId){
@@ -100,5 +185,43 @@ public class DSSVNhapDiemActivity extends AppCompatActivity {
 
         }
         return res;
+    }
+
+    private Map<String, Pair<String, Boolean>> getStudentInClass(String classId){
+        Map<String, Pair<String, Boolean>> res = new HashMap<String, Pair<String, Boolean>>();
+        String id = "";
+        try{
+            Cursor dt = DBhelper.GetData("SELECT * FROM student");
+            while(dt.moveToNext()){
+                res.put(dt.getString(0), Pair.create(dt.getString(1), false));
+
+            }
+
+            dt = DBhelper.GetData("SELECT studentid FROM score WHERE classid = '" + classId + "'");
+            while(dt.moveToNext()){
+                id = dt.getString(0);
+                res.put(id, Pair.create(res.get(id).first, true));
+                Log.d("MSVVV", dt.getString(0));
+            }
+        }catch (Exception e){
+
+        }
+        return res;
+    }
+
+    private void addStudentIntoClass (String studentId, String classId){
+        try{
+            DBhelper.QueryData("insert or replace into score values('" + studentId + "','" + classId +"',NULL)");
+        }catch (Exception e){
+
+        }
+    }
+
+    private void removeStudentFromClass (String studentId, String classId){
+        try{
+            DBhelper.QueryData("delete FROM score where score.studentid = '" + studentId + "' and score.classid = '" + classId + "'");
+        }catch (Exception e){
+
+        }
     }
 }
